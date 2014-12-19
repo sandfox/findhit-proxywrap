sandfox/findhit-proxywrap

commit d3599dca91267b8e9ad6beba1f30134f7f9e3958

Add Regexp Proxy header validation
closes #6

lib/proxywrap.js
@@ -1,18 +1,18 @@
 /*
  * node-proxywrap
- * 
+ *
  * Copyright (c) 2013, Josh Dague
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
- * 
+ * modification, are permitted provided that the following conditions are met:
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution. 
- * 
+ *    and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -26,7 +26,8 @@
  */
 
 var util = require( 'util' ),
-	Util = require( 'findhit-util' );
+	Util = require( 'findhit-util' ),
+	ProxyProtocolRegexp = require( './proxy-protocol.regexp.js' );
 
 //var legacy = !require('stream').Duplex;  // TODO: Support <= 0.8 streams interface
 
@@ -93,15 +94,15 @@ exports.proxy = function(iface, options) {
 		// override the socket's event emitter so we can process data (and discard the PROXY protocol header) before the underlying Server gets it
 		socket.emit = function(event, data) {
 			history.push(Array.prototype.slice.call(arguments));
-			/*if (event == 'data') {
-				console.log('got a data event :(');
-				socket.destroy();
-			} else*/ if (event == 'readable') {
+
+			if (event == 'readable') {
 				onReadable();
 			}
 		}
 
 		function restore() {
+			if( socket.emit === realEmit ) return;
+
 			//if (legacy) socket.removeListener('data', ondata);
 			// restore normal socket functionality, and fire any events that were emitted while we had control of emit()
 			socket.emit = realEmit;
@@ -112,9 +113,19 @@ exports.proxy = function(iface, options) {
 			history = null;
 		}
 
+		function destroy( message ) {
+			message = message || 'PROXY protocol error';
+
+			protocolError = true;
+
+			socket.destroy( message );
+			restore();
+		}
+
 		socket.on('readable', onReadable);
 
 		var header = '', buf = new Buffer(0);
+
 		function onReadable() {
 			var chunk;
 			while (null != (chunk = socket.read())) {
@@ -123,10 +134,8 @@ exports.proxy = function(iface, options) {
 
 				// if the first 5 bytes aren't PROXY, something's not right.
 				if (header.length >= 5 && header.substr(0, 5) != 'PROXY') {
-					protocolError = true;
 					if (options.strict) {
-						socket.destroy('PROXY protocol error');
-						return restore();
+						return destroy( 'PROXY protocol error' );
 					}
 				}
 
@@ -135,10 +144,15 @@ exports.proxy = function(iface, options) {
 					socket.removeListener('readable', onReadable);
 					header = header.substr(0, crlf);
 
+					// Check if header is valid
+					if( ! ProxyProtocolRegexp.test( header ) ) {
+						return destroy( "PROXY protocol error" );
+					}
+
 					var hlen = header.length;
 					header = header.split(' ');
-	
-					if (!protocolError) {
+
+					if ( ! protocolError ) {
 
 						if( options.overrideRemote ) {
 
@@ -159,7 +173,7 @@ exports.proxy = function(iface, options) {
 
 						}
 
-						// Source properties of TCP connection 
+						// Source properties of TCP connection
 						Object.defineProperty(socket, 'clientAddress', {
 							enumerable: false,
 							configurable: true,
@@ -175,7 +189,7 @@ exports.proxy = function(iface, options) {
 							}
 						});
 
-						// Destination properties of TCP connection 
+						// Destination properties of TCP connection
 						Object.defineProperty(socket, 'proxyAddress', {
 							enumerable: false,
 							configurable: true,
@@ -200,15 +214,19 @@ exports.proxy = function(iface, options) {
 
 					restore();
 
-					if (socket.ondata) {
+					if ( socket.ondata ) {
 						var data = socket.read();
-						if (data) socket.ondata(data, 0, data.length);
+
+						if ( data ) {
+							socket.ondata(data, 0, data.length);
+						}
 					}
 
 					break;
 
+				} else if ( header.length > 107 ) {
+					return destroy('PROXY header too long');
 				}
-				else if (header.length > 107) return socket.destroy('PROXY protocol error'); // PROXY header too long
 			}
 		}
 

lib/proxy-protocol.regexp.js
@@ -1,0 +1,95 @@
+/*
+[source](http://www.haproxy.org/download/1.5/doc/proxy-protocol.txt)
+
+2.1
+
+This is the format specified in version 1 of the protocol. It consists in one
+line of ASCII text matching exactly the following block, sent immediately and
+at once upon the connection establishment and prepended before any data flowing
+from the sender to the receiver :
+
+- a string identifying the protocol : "PROXY" ( \x50 \x52 \x4F \x58 \x59 )
+Seeing this string indicates that this is version 1 of the protocol.
+
+- exactly one space : " " ( \x20 )
+
+- a string indicating the proxied INET protocol and family. As of version 1,
+only "TCP4" ( \x54 \x43 \x50 \x34 ) for TCP over IPv4, and "TCP6"
+( \x54 \x43 \x50 \x36 ) for TCP over IPv6 are allowed. Other, unsupported,
+or unknown protocols must be reported with the name "UNKNOWN" ( \x55 \x4E
+\x4B \x4E \x4F \x57 \x4E ). For "UNKNOWN", the rest of the line before the
+CRLF may be omitted by the sender, and the receiver must ignore anything
+presented before the CRLF is found. Note that an earlier version of this
+specification suggested to use this when sending health checks, but this
+causes issues with servers that reject the "UNKNOWN" keyword. Thus is it
+now recommended not to send "UNKNOWN" when the connection is expected to
+be accepted, but only when it is not possible to correctly fill the PROXY
+line.
+
+- exactly one space : " " ( \x20 )
+
+- the layer 3 source address in its canonical format. IPv4 addresses must be
+indicated as a series of exactly 4 integers in the range [0..255] inclusive
+written in decimal representation separated by exactly one dot between each
+other. Heading zeroes are not permitted in front of numbers in order to
+avoid any possible confusion with octal numbers. IPv6 addresses must be
+indicated as series of 4 hexadecimal digits (upper or lower case) delimited
+by colons between each other, with the acceptance of one double colon
+sequence to replace the largest acceptable range of consecutive zeroes. The
+total number of decoded bits must exactly be 128. The advertised protocol
+family dictates what format to use.
+
+- exactly one space : " " ( \x20 )
+
+- the layer 3 destination address in its canonical format. It is the same
+format as the layer 3 source address and matches the same family.
+
+- exactly one space : " " ( \x20 )
+
+- the TCP source port represented as a decimal integer in the range
+[0..65535] inclusive. Heading zeroes are not permitted in front of numbers
+in order to avoid any possible confusion with octal numbers.
+
+- exactly one space : " " ( \x20 )
+
+- the TCP destination port represented as a decimal integer in the range
+[0..65535] inclusive. Heading zeroes are not permitted in front of numbers
+in order to avoid any possible confusion with octal numbers.
+
+- the CRLF sequence ( \x0D \x0A )
+
+
+The maximum line lengths the receiver must support including the CRLF are :
+- TCP/IPv4 :
+"PROXY TCP4 255.255.255.255 255.255.255.255 65535 65535\r\n"
+=> 5 + 1 + 4 + 1 + 15 + 1 + 15 + 1 + 5 + 1 + 5 + 2 = 56 chars
+
+- TCP/IPv6 :
+"PROXY TCP6 ffff:f...f:ffff ffff:f...f:ffff 65535 65535\r\n"
+=> 5 + 1 + 4 + 1 + 39 + 1 + 39 + 1 + 5 + 1 + 5 + 2 = 104 chars
+
+- unknown connection (short form) :
+"PROXY UNKNOWN\r\n"
+=> 5 + 1 + 7 + 2 = 15 chars
+
+- worst case (optional fields set to 0xff) :
+"PROXY UNKNOWN ffff:f...f:ffff ffff:f...f:ffff 65535 65535\r\n"
+=> 5 + 1 + 7 + 1 + 39 + 1 + 39 + 1 + 5 + 1 + 5 + 2 = 107 chars
+
+*/
+
+var IPv4 = '(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))',
+    IPv6 = '(([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4][0-9]|1[0-9]|[1-9]?[0-9])(.(25[0-5]|2[0-4][0-9]|1[0-9]|[1-9]?[0-9])){3}))|:))',
+    Port = '([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-2][0-9]{2}|653[0-2][0-9]|6533[0-5])'; // 1..65335
+
+
+var ProxyProtocolRegexp = new RegExp(
+    '^PROXY (' +
+        'UNKNOWN' + '|' +
+        [ 'TCP4', IPv4, IPv4, Port, Port ].join(' ') +'|'+
+        [ 'TCP6', IPv6, IPv6, Port, Port ].join(' ') +
+    ')$'
+);
+
+// Export it
+module.exports = ProxyProtocolRegexp;

test/proxy.protocol.v1.test.js
@@ -31,12 +31,12 @@ describe( "PROXY Protocol v1", function () {
 			return tUtil.fakeConnect(
 				server,
 				{
-					header: 'PROXY 192.168.0.1 192.168.0.254 3350 443',
+					header: 'PROXY TCP4 192.168.0.1 192.168.0.254 3350 443',
 				}
 			);
 		});
 
-		describe( "Should detect a malformed PROXY header", function () {
+		describe( "Should detect a malformed PROXY headers", function () {
 
 			it( "Header without IP's", function () {
 				var self = this;

commit 53f470fc319e9498d3fd1285019cc1e0cfdbe9f7

fixes situation where if using non-strict mode and a non proxy protocol connection comes in everything blows up. This has probably been broken since d3599dca91267b8e9ad6beba1f30134f7f9e3958

lib/proxywrap.js
@@ -134,6 +134,7 @@ exports.proxy = function(iface, options) {
 
 				// if the first 5 bytes aren't PROXY, something's not right.
 				if (header.length >= 5 && header.substr(0, 5) != 'PROXY') {
+					protocolError = true;
 					if (options.strict) {
 						return destroy( 'PROXY protocol error' );
 					}